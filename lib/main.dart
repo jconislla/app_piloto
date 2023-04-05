4598db9e67addd2c9c4f92a992af57baa8f000c7
Jair Pool Conislla Bocangel <jconislla@csticorp.biz>
2023-04-05 10:23:06 -0500

    feat: update main

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const ChannelView();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ChannelView(),
+    );
   }
 }
